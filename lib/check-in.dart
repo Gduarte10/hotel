7a4f19118dffc21e7d51fed0aac1499ca9b37949
gabriel <duartegabriel0102@gmail.com>
2023-03-09 10:29:41 -0300

    first commit

---
+++ lib/check-in.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:hotel/model/user_model.dart';
+
+class Checkin extends StatefulWidget {
+  const Checkin({Key? key}) : super(key: key);
+
+  @override
+  _CheckinState createState() => _CheckinState();
+}
+
+class _CheckinState extends State<Checkin> {
+  bool _toggleVisibility = true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController contatoController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    senhaController.dispose();
+    nomeController.dispose();
+    contatoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModelDescendant<UserModel>(
+      builder: (context, child, model) => Scaffold(
+        appBar: AppBar(
+          title: Text('Registro'),
+          backgroundColor: const Color(0xFF3CB371), // cor verde pastel
+        ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: nomeController,
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 16,
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Digite seu nome' : null,
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 16,
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Digite seu email' : null,
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: senhaController,
+                  obscureText: _toggleVisibility,
+                  decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _toggleVisibility = !_toggleVisibility;
+                          });
+                        },
+                        icon: _toggleVisibility
+                            ? const Icon(
+                                Icons.visibility_off,
+                                color: Colors.grey,
+                              )
+                            : const Icon(
+                                Icons.visibility,
+                                color: Colors.grey,
+                              ),
+                      )),
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 16,
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Digite sua senha' : null,
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: contatoController,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    labelText: 'Contato',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 16,
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Digite seu contato' : null,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => Navigator.pushNamed(context, '/login'),
+                  child: Text(
+                    'Já possui uma conta? Faça login',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: const Color(0xFF3CB371), // cor verde pastel
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    elevation: 0,
+                    padding: const EdgeInsets.all(0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      side: BorderSide(
+                        color: const Color(0xFF3CB371), // cor verde pastel
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      model.cadastroUser(
+                        nome: nomeController.text,
+                        email: emailController.text,
+                        senha: senhaController.text,
+                        contato: contatoController.text,
+                        onSuccess: _onSuccess,
+                        onFail: _onFail,
+                      );
+                    }
+                  },
+                  child: Text(
+                    'Cadastrar',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xFF3CB371), // cor verde pastel
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onSuccess() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const Teste()),
+    );
+  }
+
+  void _onFail() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Falha no registro!'),
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+}
